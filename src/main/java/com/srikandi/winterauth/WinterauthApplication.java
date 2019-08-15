b50f7f596f57bfbe964ab4f23858d2a267694900
Otniel <otniel.mark@gmail.com>
2019-08-15 20:56:42 +0700

    initial push

---
+++ src/main/java/com/srikandi/winterauth/WinterauthApplication.java
@@ -0,0 +1,13 @@
+package com.srikandi.winterauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinterauthApplication.class, args);
+	}
+
+}
